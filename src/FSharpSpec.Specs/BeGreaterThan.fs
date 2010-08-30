9731106db01a9ca35069d0065d0138ff60e0af20
Thorsten Lorenz <tlorenz@lab49.com>
2010-08-29 21:20:19 -0400

    Cleaning up.

--- src/FSharpSpec.Specs/BeGreaterThan.fs
+++ src/FSharpSpec.Specs/BeGreaterThan.fs
@@ -5,7 +5,7 @@ open FSharpSpec
 
 type ``BeGreaterThan`` () =
     static member ``Structs`` = [
-        it "1 should.beGreaterThan 0" 1 should.beGreaterThan 2
+        it "1 should.beGreaterThan 0" 1 should.beGreaterThan 0
         it "0 should.beGreaterThan -1" 0 should.beGreaterThan -1
         
         ass "1 should.beGreaterThan 1 will fail" (it "" 1 should.beGreaterThan 1) will.fail
