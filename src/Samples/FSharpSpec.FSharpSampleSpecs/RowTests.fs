0d6777e299b10330c503bfc75804dcd0ea3a99f1
Thorsten Lorenz <tlorenz@lab49.com>
2010-09-06 19:42:21 -0400

    Rowtests for Math.Abs

--- src/Samples/FSharpSpec.FSharpSampleSpecs/RowTests.fs
+++ src/Samples/FSharpSpec.FSharpSampleSpecs/RowTests.fs
@@ -6,17 +6,19 @@ open FSharpSpec
 type ``when applying Math_Abs`` () =
     
     member x.``to positive numbers`` =
-        let rec getAbsolutesSpecs (numbers : int list) = 
+        let rec getAbsForPositiveNumbersSpecs (numbers : int list) = 
             match numbers with
             | []        -> []
-            | x::xs     -> it (sformat "Abs({0}) returns {0}" x) (System.Math.Abs(x)) should.equal x :: getAbsolutesSpecs xs
+            | x::xs     -> it (sprintf "Abs(%d) returns %d" x x) (Math.Abs(x)) should.equal x 
+                           :: getAbsForPositiveNumbersSpecs xs
 
-        getAbsolutesSpecs [1; 2; 3; 4; 11; 111]
+        getAbsForPositiveNumbersSpecs [1; 2; 3; 4; 11; 111]
 
     member x.``to negative numbers`` =
-        let rec getAbsolutesSpecs (numbers : int list) = 
+        let rec getAbsForNegativeNumbersSpecs (numbers : int list) = 
             match numbers with
             | []        -> []
-            | x::xs     -> it (ssformat "Abs({0}) returns {1}" x (-x)) (System.Math.Abs(x)) should.equal (-x) :: getAbsolutesSpecs xs
+            | x::xs     -> it (sprintf "Abs(%d) returns %d"  x (-x)) (Math.Abs(x)) should.equal (-x) 
+                           :: getAbsForNegativeNumbersSpecs xs
 
-        getAbsolutesSpecs [-1; -2; -3; -4; -11; -111]
+        getAbsForNegativeNumbersSpecs [-1; -2; -3; -4; -11; -111]
--- src/Samples/FSharpSpec.FSharpSampleSpecs/SpecHelpers.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace FSharpSpec.FSharpSampleSpecs
-
-open System
-
-[<AutoOpen>]
-module SpecHelpers =
-    let sformat s (arg : obj) = String.Format(s, arg)
-    let ssformat s (arg1 : obj) (arg2 : obj) = String.Format(s, arg1, arg2)
