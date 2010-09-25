f78270ef3b24db7e19af523d844788799caac001
Thorsten Lorenz <thlorenz@gmx.de>
2010-09-25 10:13:26 -0400

    skeleton for TDNet Runner

---
+++ src/Runners/TestDriven.NET3.Runner/TestDrivenNetRunner.fs
@@ -0,0 +1,14 @@
+﻿namespace FSharpSpec.Runners
+
+open System.Reflection
+open TestDriven.Framework;
+
+type TestDrivenNetRunner() =
+    interface ITestRunner with
+        member x.RunAssembly(listener : ITestListener , assembly : Assembly) = TestRunState.NoTests
+        member x.RunMember(listener : ITestListener , assembly : Assembly, memberInfo : MemberInfo) = TestRunState.NoTests
+        member x.RunNamespace(listener : ITestListener , assembly : Assembly, nameSpace : string) = TestRunState.NoTests
+
+    
+
+    
