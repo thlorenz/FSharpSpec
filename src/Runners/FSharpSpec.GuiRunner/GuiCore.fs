9f806347f77affef15c4a3f112e4ca9095d6a9c2
Thorsten Lorenz <thlorenz@gmx.de>
2011-03-07 22:28:40 -0500

    adding viewmodelbase and ICommand implementation

---
+++ src/Runners/FSharpSpec.GuiRunner/GuiCore.fs
@@ -0,0 +1,33 @@
+﻿module GuiCore
+
+open System
+open System.ComponentModel
+open System.Windows.Input
+
+type ViewModelBase () =
+
+  // create an event object
+  let propertyChanged = new Event<PropertyChangedEventHandler,PropertyChangedEventArgs>()
+ 
+  // implement the interface INotifyPropertyChanged
+  interface INotifyPropertyChanged with
+      [<CLIEvent>]
+      member x.PropertyChanged = propertyChanged.Publish
+ 
+  // this method provides a convient way of raising the PropertyChanged event
+  member x.OnPropertyChanged(name)=
+      propertyChanged.Trigger (x, new PropertyChangedEventArgs(name))
+
+
+type ActionCommand (action : Action, canExecute : Func<bool>) = 
+  let _action = action
+  let _canExecute = canExecute
+
+  interface ICommand with
+    member x.Execute(_) = _action.Invoke()
+    member x.CanExecute(_) = _canExecute.Invoke()
+    
+    member x.add_CanExecuteChanged(eventHandler) = 
+      CommandManager.RequerySuggested.AddHandler eventHandler
+    member x.remove_CanExecuteChanged(eventHandler) = 
+      CommandManager.RequerySuggested.RemoveHandler eventHandler
--- src/Runners/FSharpSpec.GuiRunner/GuiRunner.fsx
+++ src/Runners/FSharpSpec.GuiRunner/GuiRunner.fsx
@@ -3,6 +3,9 @@
 #I @"C:\dev\FSharp\FSharpSpec\src\Runners\FSharpSpec.RunnerUtils\bin\Debug"
 #r "FSharpSpec.RunnerUtils.dll"
 
+#I @"bin\Debug\"
+#r "FSharpSpec.GuiRunner.exe"
+
 #I @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0\"
 #r @"PresentationCore.dll"
 #r "PresentationFramework.dll"
@@ -31,8 +34,27 @@ open System.Collections.ObjectModel
 open System.Windows
 open System.Windows.Controls
 
-type ContextsViewModel () =
-  let _childContexts = new ObservableCollection<ContextsViewModel>()
+open CoreGui
+
+type SpecViewModel (spec : (string * SpecDelegate)) =
+  inherit ViewModelBase ()
+
+  let _name = fst spec
+  let _delegate = snd spec
+
+  member x.Name = _name
+
+
+type SpecContainerViewModel () =
+  inherit ViewModelBase ()
+  
+  let _specifications = ObservableCollection<SpecViewModel>();
+  member x.Specifications = _specifications
+
+type ContextViewModel () = 
+  inherit ViewModelBase ()
+
+  let _childContexts = ObservableCollection<ContextViewModel>()
   let _specContainers = ObservableCollection()
   
   member x.ChildContexts = _childContexts
@@ -40,71 +62,6 @@ type ContextsViewModel () =
 
   member public x.Explore (tree : Node) = ()
 
-let runSpecsAndShowResults name printer (node : Node) = 
-  printfn "Running specs"
-  let results = node.RunSpecs() 
-  
-  printResultTree     results printer          
-  printFailureDetails results printer
-  printPendingSummary results printer
-  printFailureSummary results printer
-        
-let tree = allContexts |> getContextTreeOfContexts
 
-let uiTree = TreeView()
-let win = Window( Topmost = true, Width=300.0, Height = 200.0, Content = uiTree)
-win.Show()
-
-let rootItem = TreeViewItem ( Header = (getName asm.FullName))
-uiTree.Items.Add rootItem
-
-
-let foundInItemsOf (treeItem : TreeViewItem) name =
-  treeItem.Items.Cast<TreeViewItem>()
-  |> Seq.filter (fun i -> i.Header = name)
-
-let addOrFindItem rootItem (node : Node) name = 
-  let foundItems = name |> foundInItemsOf rootItem
-  match name with
-  | n when Seq.length foundItems > 0  -> Seq.head foundItems
-  | n                                 -> 
-      let newItem = TreeViewItem ( Header = n, DataContext = node )
-      rootItem.Items.Add newItem |> ignore
-      newItem.MouseDown |> Event.add (fun i -> if i.Source.Equals newItem then node |> runSpecsAndShowResults name writeToConsole )
-      newItem
-
-let rec populateTree (rootItem : TreeViewItem) (node : Node) =
-  
-  let ctx = node.Context
-  let clazz = ctx.Clazz
-  let moduleType = clazz.DeclaringType
-  let contextName = clazz.Name    
-
-  if (not <| clazz.Equals(typeof<obj>)) then 
-    
-    let specsContainers = ctx.SpecLists
-    
-    let contextItem = 
-      match moduleType with
-      | n when n = null ->  addOrFindItem rootItem node contextName
-      | n               ->  let moduleItem = addOrFindItem rootItem node moduleType.Name 
-                            addOrFindItem moduleItem node contextName
-
-   
-    let instantiatedContext = ctx.Clazz |> instantiate
-    specsContainers |> Array.iter (fun container ->
-      let containerItem =  container.Name  |> removeLeadingGet|> addOrFindItem contextItem node
-      let specs = container.Invoke(instantiatedContext, null) :?> (string * SpecDelegate) list
-      specs 
-      |> List.map  (fun s -> fst s)
-      |> List.map  (fun n -> TreeViewItem (Header = n))
-      |> List.iter (fun i -> containerItem.Items.Add i |> ignore)
-    )
-
-  node.Children
-  |> List.iter (fun c -> populateTree rootItem c)
-
-rootItem.Items.Clear()
-populateTree rootItem tree
 
 
