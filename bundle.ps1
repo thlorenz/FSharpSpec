fd3c9d154a4dc05ce4efa8a9ea67059209e68951
Thorsten Lorenz <thlorenz@gmx.de>
2011-04-24 14:36:39 -0400

    including more tools inside bundle

--- bundle.ps1
+++ bundle.ps1
@@ -1,6 +1,10 @@
+rm bundle -R -fo
 mkdir bundle
 cp .\src\Runners\FSharpSpec.ConsoleRunner\bin\Debug\* bundle
 cp .\src\Runners\FSharpSpec.TDNetRunner\bin\Debug\* bundle
+cp .\src\Runners\FSharpSpec.GuiRunner\bin\Debug\* bundle
+cp .\src\FSharp.Interop.NSubstitute\bin\Debug\* bundle
+cp .\src\FSharpSpec.Rx\bin\Debug\* bundle
 cd bundle
 . 7z.exe a FSharpSpec.zip *.*
 cd .. 
